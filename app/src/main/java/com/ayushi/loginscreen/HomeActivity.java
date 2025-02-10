32c55ba566f73d575d0cf33a4d69945f6f97305a
ayubonbon <ayushi.7sawant@gmail.com>
2025-02-10 22:10:26 +0530

    1

---
+++ app/src/main/java/com/ayushi/loginscreen/HomeActivity.java
@@ -0,0 +1,4 @@
+package com.ayushi.loginscreen;
+
+public class HomeActivity {
+}
--- app/src/main/java/com/ayushi/loginscreen/LoginActivity.java
+++ app/src/main/java/com/ayushi/loginscreen/LoginActivity.java
@@ -1,31 +1,19 @@
 package com.ayushi.loginscreen;
 
-import android.annotation.SuppressLint;
 import android.os.Bundle;
-
-import com.google.android.material.snackbar.Snackbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
-import android.content.Intent;
-import android.widget.Toast;
 
 
-import androidx.navigation.NavController;
-import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
-import androidx.navigation.ui.NavigationUI;
 
 import com.ayushi.loginscreen.databinding.ActivityMainBinding;
 
-import android.view.Menu;
-import android.view.MenuItem;
 
-
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
     private ActivityMainBinding binding;
@@ -40,9 +28,12 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onClick(View v) {
-
+                onRegisterClick();
             }
         });
+    }
+
+    private void onRegisterClick() {
     }
 }
 
